3ac51eadd306c070b6a0cbf4919ca356dbc21f5b
vailei <willey.oliveira12@gmail.com>
2022-12-16 02:06:32 -0300

    bgl é deixar style no index msm

--- api/index.php
+++ api/index.php
@@ -3,8 +3,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../public/css/index.css">
-    
+    <style>
+    <?php
+        define('__ROOT__', dirname(dirname(__FILE__)));
+        require_once(__ROOT__.'/public/css/index.css');
+    ?>
+    </style>
     <link rel="apple-touch-icon" sizes="180x180" href="src/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="src/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="src/favicon/favicon-16x16.png">
