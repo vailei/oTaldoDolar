5dd6baa23e37b29bd7ba7e81f2baa868e9aa93a2
vailei <willey.oliveira12@gmail.com>
2022-12-16 20:26:19 -0300

    ultima tentativa.

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../public/css/index.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="public/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../public/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="public/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="public/favicon/favicon-16x16.png">
     <link rel="manifest" href="public/favicon/site.webmanifest">
@@ -14,8 +14,8 @@
     <header>
         <div class="menu">
             <a href="https://otaldodolar.vercel.app">Dólar</a>
-            <a href="euro.php">Euro</a>
-            <a href="libra.php">Libra</a>
+            <a href="/api/euro.php">Euro</a>
+            <a href="/api/libra.php">Libra</a>
         </div>
     </header>
     <div class="main">
