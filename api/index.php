6ad52210c52ebf5da9fb837c25b79994a76f8b3f
vailei <willey.oliveira12@gmail.com>
2022-12-16 02:24:18 -0300

    perdi a conta

--- api/index.php
+++ api/index.php
@@ -1,21 +1,8 @@
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-    <?php
-        define('__ROOT__', dirname(dirname(__FILE__)));
-        require_once(__ROOT__.'/public/css/index.css');
-    ?>
-    </style>
-    <link rel="apple-touch-icon" sizes="180x180" href="public/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="public/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="public/favicon/favicon-16x16.png">
-    <link rel="manifest" href="public/favicon/site.webmanifest">
-    <title>oTalDoDolar</title>
-    
-</head>
+<?php
+    define('__ROOT__', dirname(dirname(__FILE__)));
+    require_once(__ROOT__.'/public/html/index.html');
+?>
 <body>
     <header>
         <div class="menu">
