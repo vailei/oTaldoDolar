2f79d8d6791fb4b2a4c2a50c81763fe44d73d311
vailei <willey.oliveira12@gmail.com>
2022-12-16 18:54:43 -0300

    teste e testes

--- api/index.php
+++ api/index.php
@@ -3,16 +3,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/index.css">
     <style>
     <?php
         define('__ROOT__', dirname(dirname(__FILE__)));
         require_once(__ROOT__.'/public/css/index.css');
     ?>
     </style>
-    <link rel="apple-touch-icon" sizes="180x180" href="public/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="public/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="public/favicon/favicon-16x16.png">
-    <link rel="manifest" href="public/favicon/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="../public/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../public/favicon/site.webmanifest">
     <title>oTalDoDolar</title>
 </head>
 <body>
