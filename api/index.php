b0fc4b0b310e3c7f2b8c57bfe67ae13cee1c5a9f
vailei <willey.oliveira12@gmail.com>
2022-12-16 01:59:50 -0300

    teste 6

--- api/index.php
+++ api/index.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../public/css/index.css">
     <?php
-        require_once('../public/css/index.css');
+        define('__ROOT__', dirname(dirname(__FILE__)));
+        require_once(__ROOT__.'/public/css/index.css');
     ?>
     <link rel="apple-touch-icon" sizes="180x180" href="src/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="src/favicon/favicon-32x32.png">
