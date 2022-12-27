54ea47b1b95720bf0119f68417fc65c6eb1f44a3
vailei <52324425+vailei@users.noreply.github.com>
2022-12-27 13:02:28 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -41,5 +41,6 @@
             ?>
         </div>
     </div>
+<script type="text/javascript"> var infolinks_pid = 3383715; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
 </body>
 </html>
