efade4ef3989d3d128a1201ebdfbfbcd59a786d7
vailei <willey.oliveira12@gmail.com>
2022-12-26 21:48:24 -0300

    versão teste concluida

--- api/libra.php
+++ api/libra.php
@@ -3,17 +3,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../public/css/index.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="public/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="public/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="public/favicon/favicon-16x16.png">
-    <link rel="manifest" href="public/favicon/site.webmanifest">
+    <link rel="stylesheet" type="text/css" href="/public/css/index.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/public/favicon/site.webmanifest">
     <title>Euro - oTalDoDolar</title>
 </head>
 <body>
     <header>
         <div class="menu">
-            <a href="https://otaldodolar.vercel.app">Dólar</a>
+            <a href="/">Dólar</a>
             <a href="euro">Euro</a>
             <a href="libra">Libra</a>
         </div>
